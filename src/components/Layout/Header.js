94748b6e89a67f38aa4db36414b6d726a931410b
Kiran K K <kirankalak4@gmail.com>
2021-08-07 20:52:15 +0530

    made the cart button, close cart button and backdrop close functionable

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,16 +1,21 @@
-import React,{Fragment} from 'react';
-import mealsImage from '../../assets/meals.jpg'
-import classes from './Header.module.css'
+import { Fragment } from 'react';
+
 import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
 const Header = (props) => {
-return <Fragment>
-    <head className={classes.header}>
-    Delicious 
-    <HeaderCartButton/>
-    </head>
-    <div className={classes['main-image']}> 
-    <img src={mealsImage} alt="A table full of delicious meals" />  
-    </div>
-</Fragment>
-}
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
+
 export default Header;
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,21 +1,16 @@
-import React ,{Fragment} from 'react';
 import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css'
+import classes from './HeaderCartButton.module.css';
 
-const HeaderCartButton = () => {
-return <Fragment>
-<button className={classes.button}>
-    <span className={classes.icon}>
+const HeaderCartButton = (props) => {
+  return (
+    <button className={classes.button} onClick={props.onClick}>
+      <span className={classes.icon}>
         <CartIcon />
-    </span>
-    <span>
-        Cart
-    </span>
-    <span className={classes.badge}>
-        3
-    </span>
-</button>
-</Fragment>
-}
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
+    </button>
+  );
+};
 
 export default HeaderCartButton;
